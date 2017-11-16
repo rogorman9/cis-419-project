cf98d55d7f0897e831845ce4172753c7dfd7eaee
Alex Matthys <alexmatthys@gmail.com>
2017-11-15 21:57:11 -0500

    calculated mean and std using numpy methods

--- extract_features_from_wav.py
+++ extract_features_from_wav.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 from scipy.io.wavfile import read
 
 
@@ -13,8 +13,19 @@ Features Returned:
 def extract_features_from_wav(file_name):
 	raw = read(file_name, 'rb')
 	audio = raw[1]
+	audio = audio[:,0:1]
+	print audio.shape
+
+	amplitude_abs_value = np.absolute(audio)
+
+	mean = np.mean(amplitude_abs_value)
+	std = np.std(amplitude_abs_value)
+
+	
+	
 
 
 
 
+extract_features_from_wav('test_Everyday.wav')
 
