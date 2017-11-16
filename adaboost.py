52a4566e3445fc98fcb0fa13be3650fa58ffa248
rogorman9 <rogorman9@gmail.com>
2017-11-15 23:58:39 -0500

    Implements adaboost

--- adaboost.py
+++ adaboost.py
@@ -1,28 +1,33 @@
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 from generate_MFCC import gen_MFCC
 import os
+import numpy as np
 
-classical_path = "data/classical"
-metal_path = "data/metal"
+classical_path = os.path.join("data", "classical")
+metal_path = os.path.join("data", "metal")
 
 X = []
 y = []
 
 for audio_file in os.listdir(classical_path):
-    print gen_MFCC(os.path.join(classical_path, audio_file))
-    X.append(gen_MFCC(os.path.join(classical_path, audio_file)).flatten())
+    print audio_file
+    X.append(gen_MFCC(os.path.join(classical_path, audio_file)).flatten().tolist())
     y.append("classical")
 
 for audio_file in os.listdir(metal_path):
-    X.append(gen_MFCC(os.path.join(metal_path, audio_file)).flatten())
+    print audio_file
+    X.append(gen_MFCC(os.path.join(metal_path, audio_file)).flatten().tolist())
     y.append("metal")
+
+X = np.array(X)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 model = AdaBoostClassifier()
-model.fit(X_train, y_train)
-print model.score(X_test, y_test)
+
+print cross_val_score(model, X, y, cv=5)
 
 
 
