c6df45e2bb795004b7bbdbc302c5fc9d5c893adc
Alex Matthys <alexmatthys@gmail.com>
2017-11-15 22:26:34 -0500

    analysis on proprotion of amplitude data that is less than 100

--- extract_features_from_wav.py
+++ extract_features_from_wav.py
@@ -5,27 +5,44 @@ from scipy.io.wavfile import read
 """
 Function to convert wav file into feature vector.
 
-Split song into quarters, only analyze 2nd and 3rd quarters.
-
 Features Returned:
 	- mean of absolute value of amplitude data
+	- std of absolute value of amplitude data
+	- variance of absolute value of amplitude data
+	- we will split the song into 100 segments, take the mean amplitude for each segment, and take the std of these 100 means
+		- this will give us a good measure of the consistency of the song volume (for example, if there are loud choruses and soft verses)
 """
 def extract_features_from_wav(file_name):
 	raw = read(file_name, 'rb')
 	audio = raw[1]
-	audio = audio[:,0:1]
 	print audio.shape
 
 	amplitude_abs_value = np.absolute(audio)
 
+	# under25 = 0
+	# between25and100 = 0
+	# zero = 0
+	# for indx,val in enumerate(amplitude_abs_value):
+	# 	if val < 25 and val > 0:
+	# 		under25 += 1
+	# 	elif val > 24 and val < 101:
+	# 		between25and100 += 1
+	# 	elif val < 1:
+	# 		zero += 1
+	# print under25, between25and100, zero
+
 	mean = np.mean(amplitude_abs_value)
 	std = np.std(amplitude_abs_value)
+	var = np.var(amplitude_abs_value)
+
+	avg_of_100_segments = np.array([])
+
+	print mean, std, var
 
 	
-	
 
 
 
 
-extract_features_from_wav('test_Everyday.wav')
+extract_features_from_wav('data/metal/cleaned/Black_Sabbath_War_Pigs.wav')
 
