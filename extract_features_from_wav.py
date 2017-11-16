c3967df638ca2c2dccef3eabeaef27f895b0929c
Alex Matthys <alexmatthys@gmail.com>
2017-11-15 22:58:39 -0500

    updated path in plot_amp.py file

--- extract_features_from_wav.py
+++ extract_features_from_wav.py
@@ -2,17 +2,18 @@ import numpy as np
 from scipy.io.wavfile import read
 
 
-"""
-Function to convert wav file into feature vector.
-
-Features Returned:
-	- mean of absolute value of amplitude data
-	- std of absolute value of amplitude data
-	- variance of absolute value of amplitude data
-	- we will split the song into 100 segments, take the mean amplitude for each segment, and take the std of these 100 means
-		- this will give us a good measure of the consistency of the song volume (for example, if there are loud choruses and soft verses)
-"""
 def extract_features_from_wav(file_name):
+	"""
+	Function to convert wav file into feature vector.
+
+	Features Returned:
+		- mean of absolute value of amplitude data
+		- std of absolute value of amplitude data
+		- variance of absolute value of amplitude data
+		- we will split the song into 100 segments, take the mean amplitude for each segment, and take the std of these 100 means
+			- this will give us a good measure of the consistency of the song volume (for example, if there are loud choruses and soft verses)
+	"""
+
 	raw = read(file_name, 'rb')
 	audio = raw[1]
 
@@ -42,18 +43,18 @@ def extract_features_from_wav(file_name):
 
 	std_of_100_avgs = np.std(avg_of_100_segments)
 
-	print str(file_name), mean, std, var, std_of_100_avgs
+	return (str(file_name), mean, std, var, std_of_100_avgs)
 
 	
 
 
 
 
-extract_features_from_wav('data/metal/Black_Sabbath_War_Pigs.wav')
-extract_features_from_wav('data/metal/Black_Sabbath_Iron_Man.wav')
-extract_features_from_wav('data/metal/Black_Sabbath_Paranoid.wav')
-extract_features_from_wav('data/metal/Holy_Wars_The_Punishment_Due.wav')
-extract_features_from_wav('data/metal/Iron_Maiden_Hallowed_Be_Thy_Name.wav')
+# extract_features_from_wav('data/metal/Black_Sabbath_War_Pigs.wav')
+# extract_features_from_wav('data/metal/Black_Sabbath_Iron_Man.wav')
+# extract_features_from_wav('data/metal/Black_Sabbath_Paranoid.wav')
+# extract_features_from_wav('data/metal/Holy_Wars_The_Punishment_Due.wav')
+# extract_features_from_wav('data/metal/Iron_Maiden_Hallowed_Be_Thy_Name.wav')
 
 
 
