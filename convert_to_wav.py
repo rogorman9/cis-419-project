e6c7e3fc4d4fe2c3954e44b0273c45856ac3e4c9
rogorman9 <rogorman9@gmail.com>
2017-12-01 17:00:16 -0500

    Adds script for mp3 to wav conversion

---
+++ convert_to_wav.py
@@ -0,0 +1,24 @@
+"""
+Usage: convert_to_wav.py <file or directory name>
+
+Converts a file or directory of files from mp3 to mono wav and saves them to
+the same location. This script requires ffmpeg to be installed.
+"""
+
+from pydub import AudioSegment
+import os
+import sys
+
+if len(sys.argv) < 2:
+    print "Usage: python convert_to_wav.py <file or directory name>"
+    sys.exit(0)
+
+filename = sys.argv[1]
+
+if os.path.isdir(filename):
+    for file in os.listdir(filename):
+        song = AudioSegment.from_mp3(file).set_channels(1)
+        song.export(file.rsplit('.', 1)[0] + ".wav", format='wav', parameters=["-ac", "1"])
+else:
+    song = AudioSegment.from_mp3(filename).set_channels(1)
+    song.export(filename.rsplit('.', 1)[0] + ".wav", format='wav', parameters=["-ac", "1"])
