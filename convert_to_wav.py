b28e5c67a35a2baf845b18360fe8db120352410e
rogorman9 <rogorman9@gmail.com>
2017-12-01 17:26:15 -0500

    Reconverts wav files

--- convert_to_wav.py
+++ convert_to_wav.py
@@ -18,7 +18,7 @@ filename = sys.argv[1]
 if os.path.isdir(filename):
     for file in [os.path.join(filename, f) for f in os.listdir(filename)]:
         song = AudioSegment.from_mp3(file).set_channels(1)
-        song.export(file.rsplit('.', 1)[0] + ".wav", format='wav', parameters=["-ac", "1"])
+        song.export(file.rsplit('.', 1)[0] + ".wav", format='wav')
 else:
     song = AudioSegment.from_mp3(filename).set_channels(1)
-    song.export(filename.rsplit('.', 1)[0] + ".wav", format='wav', parameters=["-ac", "1"])
+    song.export(filename.rsplit('.', 1)[0] + ".wav", format='wav')
