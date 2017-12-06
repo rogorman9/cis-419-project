81a2f04d20244c41aff2d02b3614b562dd77a1fb
rogorman9 <rogorman9@gmail.com>
2017-12-05 20:55:23 -0500

    Adds rap to adaboost

--- adaboost.py
+++ adaboost.py
@@ -5,21 +5,17 @@ from generate_MFCC import gen_MFCC
 import os
 import numpy as np
 
-classical_path = os.path.join("data", "classical")
-metal_path = os.path.join("data", "metal")
+genres = ["classical", "metal", "rap"]
 
 X = []
 y = []
 
-for audio_file in os.listdir(classical_path):
-    print audio_file
-    X.append(gen_MFCC(os.path.join(classical_path, audio_file)).flatten().tolist())
-    y.append("classical")
-
-for audio_file in os.listdir(metal_path):
-    print audio_file
-    X.append(gen_MFCC(os.path.join(metal_path, audio_file)).flatten().tolist())
-    y.append("metal")
+for genre in genres:
+    path = os.path.join("data", genre)
+    for audio_file in os.listdir(path):
+        print audio_file
+        X.append(gen_MFCC(os.path.join(path, audio_file)).flatten().tolist())
+        y.append(genre)
 
 X = np.array(X)
 
